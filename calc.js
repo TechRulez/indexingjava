f32a1906ebf67ecb07e795de9f6eb9e1edc734eb
TechRulez <joshuaosewa0@gmail.com>
2023-08-22 12:36:45 +0100

    Calc updt1

--- calc.js
+++ calc.js
@@ -1,8 +1,40 @@
 let display = document.getElementById("display")
+//display.value = 0
 
+// let one = document.getElementById("one")
 
-let one = document.getElementById("one")
+// one.addEventListener("click",()=>{
+//     display.value += one.value
+// })
 
-one.addEventListener("click",()=>{
-    display.value += one.value
+//for selecting all buttons
+let allbtn = document.querySelectorAll(".btn")
+
+console.log(allbtn);
+
+allbtn.forEach((btn)=>{
+    btn.addEventListener("click",()=>{
+        display.value += btn.value
+    })
+})
+
+let clear = document.getElementById("clear")
+
+clear.addEventListener("click",()=>{
+    display.value = ""
+})
+
+let firstValue = ""
+let add = document.getElementById("add")
+
+add.addEventListener("click",()=>{
+    firstValue = parseInt(display.value)
+    display.value = ""
+})
+
+let eqto = document.getElementById("eqto")
+
+eqto.addEventListener("click",()=>{
+    let total = parseInt(firstValue) + parseInt(display.value)
+    display.value = total
 })
